4ae5a0d3a4b48ddcd6f2f91f010db7086b5f1a3d
srihareeshh <srihareeshh@gmail.com>
2026-09-23 16:26:35 +0530

    Fixed frontendcheck python code

--- frontend_check.py
+++ frontend_check.py
@@ -1,4 +1,6 @@
 import time
 print("Running frontend checks...")
 time.sleep(4)
+with open("frontend_report.txt", "w") as f:
+    f.write("Frontend checks passed successfully.")
 print("Frontend checks passed.")
